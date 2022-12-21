631f841d694b6b2d4434a818322b71500d597efb
Petr Kočí <petr.koci@samizdat.cz>
2022-12-21 21:09:43 +0100

    data import

--- pages/scatter-salat.tsx
+++ pages/scatter-salat.tsx
@@ -1,25 +1,22 @@
 import { ScatterPlot } from "../components";
 import React from "react";
-import prumery from "../data/scatter-salat-prumery.json";
 import data from "../data/scatter-salat.json";
 import ingred from "../data/scatter-salat-ingred.json";
 import kraje from "../data/scatter-salat-kraje.json";
 import { includes } from "lodash";
 
 const series = [
-  {
-    data: prumery.map((item, index) => {
-      return { x: item * 100, y: index };
-    }),
-    name: "Česká republika",
-  },
   ...kraje.map((kraj, index) => {
     return {
       data: data.map((item, i) => {
         return { x: item[index] * 100, y: i };
       }),
       name: kraj,
-      visible: ["Hlavní město Praha", "Jihomoravský kraj"].includes(kraj)
+      visible: [
+        "Česká republika",
+        "Hlavní město Praha",
+        "Kraj Vysočina",
+      ].includes(kraj)
         ? true
         : false,
     };
